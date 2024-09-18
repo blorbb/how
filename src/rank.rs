164e492fec5a60d05dcdd0d04dfb612696197456
blorbb <88137137+blorbb@users.noreply.github.com>
2024-09-18 22:27:55 +1000

    better ranking weightings

--- src/rank.rs
+++ src/rank.rs
@@ -6,10 +6,17 @@ use rust_fuzzy_search::fuzzy_compare;
 use crate::db::Entry;
 
 pub fn rank<'a>(query: &str, entries: &'a [Rc<Entry>]) -> Vec<(usize, f32)> {
+    let query = query.to_lowercase();
     let mut matches = entries
         .iter()
         .enumerate()
-        .map(|(i, entry)| (i, fuzzy_compare(query, &entry.to_haystack())))
+        .map(|(i, entry)| {
+            // varying weightings for each
+            let title_cmp = fuzzy_compare(&query, &entry.title().to_lowercase());
+            let desc_cmp = fuzzy_compare(&query, &entry.description().to_lowercase());
+            let ans_cmp = fuzzy_compare(&query, &entry.answer().to_lowercase());
+            (i, title_cmp * 2.0 + desc_cmp + ans_cmp * 1.5)
+        })
         .collect_vec();
     matches.sort_by(|a, b| b.1.total_cmp(&a.1));
     matches
