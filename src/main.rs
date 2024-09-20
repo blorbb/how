36890a98bd3efee126aff8b1d2eeb7faad0009cf
blorbb <88137137+blorbb@users.noreply.github.com>
2024-09-20 17:22:07 +1000

    print to stderr

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ mod ui;
 mod utils;
 mod widgets;
 
-use std::fs;
+use std::{
+    fs,
+    io::{self, stderr, BufWriter},
+};
 
 use color_eyre::{
     eyre::{Context, ContextCompat},
     Result,
 };
-use crossterm::event::{self, Event, KeyEventKind};
+use crossterm::{
+    event::{self, Event, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
 use db::Data;
+use ratatui::{prelude::CrosstermBackend, Terminal};
 use ui::App;
+
 fn main() -> Result<()> {
     let dir = dirs::data_dir().context("unable to find data directory")?;
 
@@ -23,7 +32,12 @@ fn main() -> Result<()> {
         .open(dir.join("how-db.toml"))
         .context("unable to open how-db.toml")?;
 
-    let mut terminal = ratatui::init();
+    // https://ratatui.rs/faq/#should-i-use-stdout-or-stderr
+    // same as `ratatui::restore()` but with stderr instead.
+    set_panic_hook();
+    enable_raw_mode()?;
+    stderr().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(BufWriter::new(stderr())))?;
     terminal.clear()?;
 
     let data = Data::load_from(file)?;
@@ -42,11 +56,25 @@ fn main() -> Result<()> {
         }
     };
 
-    ratatui::restore();
+    restore()?;
 
     if let Some(s) = output {
         println!("{s}");
     }
 
+    Ok(())
+}
+
+fn set_panic_hook() {
+    let hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |info| {
+        _ = restore();
+        hook(info);
+    }));
+}
+
+fn restore() -> io::Result<()> {
+    disable_raw_mode()?;
+    stderr().execute(LeaveAlternateScreen)?;
     Ok(())
 }
