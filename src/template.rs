44ab7ccc56cbe6e2fd126ca3fdcecfdb765f9895
blorbb <88137137+blorbb@users.noreply.github.com>
2024-09-28 13:31:45 +1000

    Template syntax changes

--- src/template.rs
+++ src/template.rs
@@ -2,28 +2,30 @@
 //!
 //! Snippet is generally of the form:
 //! ```sh
-//! git diff {main#1}..{#2}
+//! git diff [main#from]..[#to]
 //! ```
-//! - Templates are wrapped in braces.
+//! - Templates are wrapped in brackets.
 //! - Contains a default before the #.
+//! - Contains a description after the #.
+//! - [] and # can be escaped with `\`. Escapes can be re-escaped like `\\`
+//!   for a literal backslash. This is only needed if there needs to be a
+//!   backslash before a `[`, `]`, or `#` for some reason - all other
+//!   backslashes that are followed by any other character will be treated
+//!   as a literal backslash.
+//!
+//! A second hash will also add an index (starting from 1):
+//! ```sh
+//! git diff {main#from#1}..{#to#2}
+//! ```
 //! - Can contain an optional number after the # for the tab order.
 //!   If no `#num` is provided, it will just go left to right, filling
 //!   in gaps between numbers. For example:
 //!   ```sh
-//!   some-command {a} {b#3} {c} {d}
+//!   some-command {a} {b##3} {c} {d}
 //!   ```
 //!   Will cycle in the order `a`, `c`, `b`, `d`.
 //! - Multiple templates can have the same number, in which case both will
 //!   be selected and edited at the same time.
-//! - {} and # can be escaped with `\`. Escapes can be re-escaped like `\\`
-//!   for a literal backslash. This is only needed if there needs to be a
-//!   backslash before a `{}#` for some reason - all other backslashes that
-//!   are followed by any other character will be treated as a literal backslash.
-//!
-//! A second hash will also add a description:
-//! ```sh
-//! git diff {main#1#from}..{#2#to}
-//! ```
 
 use std::{collections::HashMap, mem, ops::Range};
 
@@ -36,7 +38,7 @@ mod ir {
 
     use super::Error;
 
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
     pub struct IncrementalU8(u8);
 
     impl IncrementalU8 {
@@ -76,22 +78,28 @@ mod ir {
         Literal(usize),
         /// Where the default value of the input started
         Default(usize),
-        // None if no index is actually given (inferred)
-        Index(Range<usize>, Option<IncrementalU8>),
-        Description(Range<usize>, Option<IncrementalU8>, String),
+        /// Range of the default text in the *display* text;
+        /// Description.
+        Description(Range<usize>, String),
+        /// Range of the default text;
+        /// Description;
+        /// Optional index (only `None` when no number provided).
+        Index(Range<usize>, String, Option<IncrementalU8>),
     }
 }
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("found brace within brace: escape at least one to clarify intent")]
-    BraceInBrace,
-    #[error("unbalanced brace templates: escape braces that are to be treated as literals")]
-    UnbalancedBraces,
+    #[error("found bracket within bracket: escape at least one to clarify intent")]
+    BracketInBracket,
+    #[error("unbalanced bracket templates: escape braces that are to be treated as literals")]
+    UnbalancedBrackets,
     #[error("invalid input index: must be a positive integer")]
     InvalidNumber,
     #[error("invalid input index: number too large")]
     OverflowingNumber,
+    #[error("no input index given: remove the second '#' for automatic index")]
+    MissingNumber,
     #[error("too many hashes in input: escape #'s that are to be treated as literals")]
     TooManyFields,
 }
@@ -110,7 +118,7 @@ fn parse(s: &str) -> Result<TemplatedCommand, Error> {
     for c in s.chars() {
         match (c, &mut input_state) {
             // handle escape characters
-            ('{' | '}' | '#' | '\\', _) if is_escaped => {
+            ('[' | ']' | '#' | '\\', _) if is_escaped => {
                 is_escaped = false;
                 to_push = Some(c);
             }
@@ -120,7 +128,7 @@ fn parse(s: &str) -> Result<TemplatedCommand, Error> {
             }
 
             // starting a new input field with {
-            ('{', State::Literal(start)) => {
+            ('[', State::Literal(start)) => {
                 let literal_range = *start..template.display.len();
                 if !literal_range.is_empty() {
                     template
@@ -129,26 +137,23 @@ fn parse(s: &str) -> Result<TemplatedCommand, Error> {
                 };
                 input_state = State::Default(template.display.len());
             }
-            ('{', _) => return Err(Error::BraceInBrace),
+            ('[', _) => return Err(Error::BracketInBracket),
             // read first #
             ('#', State::Default(start)) => {
-                input_state = State::Index(*start..template.display.len(), None)
+                input_state = State::Description(*start..template.display.len(), String::new())
             }
             // read second #
-            ('#', State::Index(range, idx)) => {
-                // 0 is an invalid index, must be positive
-                if idx.is_some_and(|u| u.get() == 0) {
-                    return Err(Error::InvalidNumber);
-                }
-                input_state = State::Description(range.clone(), *idx, String::new())
+            ('#', State::Description(range, desc)) => {
+                input_state = State::Index(range.clone(), mem::take(desc), None)
             }
             // error on third #
-            ('#', State::Description(..)) => return Err(Error::TooManyFields),
+            ('#', State::Index(..)) => return Err(Error::TooManyFields),
             // reading # on literal is fine
+
             // closing input
-            ('}', State::Literal(..)) => return Err(Error::UnbalancedBraces),
+            (']', State::Literal(..)) => return Err(Error::UnbalancedBrackets),
             // only default value
-            ('}', State::Default(start)) => {
+            (']', State::Default(start)) => {
                 let range = *start..template.display.len();
                 unassigned_inputs.push(template.sections.len());
                 template
@@ -156,31 +161,26 @@ fn parse(s: &str) -> Result<TemplatedCommand, Error> {
                     .push(TemplateSection::Input(range, String::new()));
                 input_state = State::Literal(template.display.len());
             }
-            // index given
-            ('}', State::Index(range, idx)) => {
-                if let Some(i) = idx {
-                    input_indexes
-                        .entry(*i)
-                        .or_default()
-                        .push(template.sections.len())
-                } else {
-                    unassigned_inputs.push(template.sections.len())
-                };
+            // description given, unassigned ordering
+            (']', State::Description(range, desc)) => {
+                unassigned_inputs.push(template.sections.len());
                 template
                     .sections
-                    .push(TemplateSection::Input(range.clone(), String::new()));
+                    .push(TemplateSection::Input(range.clone(), mem::take(desc)));
                 input_state = State::Literal(template.display.len());
             }
-            // description given
-            ('}', State::Description(range, idx, desc)) => {
-                if let Some(i) = idx {
-                    input_indexes
-                        .entry(*i)
-                        .or_default()
-                        .push(template.sections.len())
-                } else {
-                    unassigned_inputs.push(template.sections.len())
-                };
+            // index given
+            // ensure there is a number assigned, error on inputs like `[a#b#]`
+            (']', State::Index(_, _, None)) => return Err(Error::InvalidNumber),
+            // fail on 0 index as well
+            (']', State::Index(_, _, Some(idx))) if idx.get() == 0 => {
+                return Err(Error::MissingNumber)
+            }
+            (']', State::Index(range, desc, Some(idx))) => {
+                input_indexes
+                    .entry(*idx)
+                    .or_default()
+                    .push(template.sections.len());
                 template
                     .sections
                     .push(TemplateSection::Input(range.clone(), mem::take(desc)));
@@ -190,8 +190,8 @@ fn parse(s: &str) -> Result<TemplatedCommand, Error> {
             // displayed text: literal or default value
             (_, State::Literal(..) | State::Default(..)) => to_push = Some(c),
             // read index of input field
-            (_, State::Index(_, ref mut idx)) => {
-                *idx = Some(idx.unwrap_or(0.into()).read_digit(c)?)
+            (_, State::Index(_, _, ref mut idx)) => {
+                *idx = Some(idx.unwrap_or_default().read_digit(c)?)
             }
             // read description of input field
             (_, State::Description(.., ref mut s)) => s.push(c),
@@ -241,7 +241,7 @@ pub enum TemplateSection {
 
 #[derive(Debug, Default)]
 pub struct TemplatedCommand {
-    display: String,
+    display: Vec<char>,
     sections: Vec<TemplateSection>,
     /// Order in which to jump to an input.
     ///
@@ -256,6 +256,6 @@ mod tests {
 
     #[test]
     pub fn works() {
-        _ = dbg!(parse("git diff {main#1#from}..{##to}"));
+        _ = dbg!(parse("git diff [main#from#1]..[#to]"));
     }
 }
